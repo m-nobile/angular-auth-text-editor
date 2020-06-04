2c95d176c010bf63f8c3c03e101ccd1d5ad9bad8
m-nobile <nobile125@gmail.com>
2020-06-03 23:10:53 -0400

    intregration of medium text editor

--- src/js/controllers/success.js
+++ src/js/controllers/success.js
@@ -1,6 +1,8 @@
 myApp.controller('SuccessController', ['$scope', function($scope) {
-  var editor = new MediumEditor('.editable');
-  $scope.onChange = function(text) {
+  var editor = new MediumEditor('.text-editor');
+
+  $scope.onChange = function() {
+    var text = document.getElementById("text").innerText;
     firebase.database().ref('users/' + $scope.currentUser.uid + "/text").set(text);
   };
 }]);
