5478b0663910da1456522821636f222ca46dfe5e
m-nobile <nobile125@gmail.com>
2020-06-12 21:28:54 -0400

    add latex equation handling

--- src/js/controllers/textEditor.js
+++ src/js/controllers/textEditor.js
@@ -3,13 +3,25 @@ myApp.controller('TextEditorController', ['$scope', function($scope) {
 
   $scope.init = function() {
     if($scope.currentUser && $scope.currentUser.text) { 
-      document.getElementById("text").innerText = $scope.currentUser.text;
+      editor.setContent($scope.currentUser.text);
     }
   }
 
   $scope.onChange = function() {
     var text = document.getElementById("text").innerText;
     firebase.database().ref('users/' + $scope.currentUser.uid + "/text").set(text);
+    //check for latex equation
+    var startDollar = text.indexOf("$");
+    if (startDollar !== -1) {
+      var endDollar = text.indexOf("$", startDollar + 1);
+      if (endDollar !== -1) {
+        var latexExpression = text.substring(startDollar + 1, endDollar);
+        var latexElement =
+          '<img src="https://latex.codecogs.com/gif.latex?' +
+          latexExpression + '" title="" style="">';
+          editor.setContent(text.replace(latexExpression, latexElement).replace(/[$]/g, ""));
+      }
+    }
   };
 
   $scope.$on('$destroy', function () {
